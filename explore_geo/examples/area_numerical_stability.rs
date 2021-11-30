2fc71bd3bed46f5ae58f13da4853f32ddac17ce9
Prathmesh Prabhu <callpraths@gmail.com>
2021-11-30 19:20:20 +0000

    Mutli-angle series

--- explore_geo/examples/area_numerical_stability.rs
+++ explore_geo/examples/area_numerical_stability.rs
@@ -2,7 +2,9 @@ use geo::{
     algorithm::area::Area, map_coords::MapCoords, CoordFloat, CoordNum, Coordinate, LineString,
     Polygon,
 };
-use std::{f64::consts::PI};
+use std::f64::consts::PI;
+use serde::Serialize;
+
 
 fn main() {
     let polygon = {
@@ -25,49 +27,63 @@ fn main() {
         )
     };
 
-    let shifts = (0..64).map(f64::from).map(|i| 1.5 * i.exp2()).collect::<Vec<f64>>();
-    let output = compute_output(&polygon, &shifts);
-    print_output(&output);
+    let shifts = (0..64)
+        .map(f64::from)
+        .map(|i| 1.5 * i.exp2())
+        .collect::<Vec<f64>>();
+    let shift_angles = [0., PI / 4., PI / 2., PI * 3. / 4., PI];
+    let series = compute_series(&polygon, &shifts, &shift_angles);
+    println!("{}", serde_json::to_string_pretty(&series).unwrap());
 }
 
-struct OutputRow {
+#[derive(Serialize)]
+struct Series {
+    pub angle: f64,
+    pub data: Vec<Datum>,
+}
+
+#[derive(Serialize)]
+struct Datum {
     pub shift: f64,
     pub original_area: f64,
     pub shifted_area: f64,
     pub shift_relative_error: f64,
-    pub naive_area_computation_error: f64
+    pub naive_area_computation_error: f64,
 }
 
-fn compute_output(polygon: &Polygon<f64>, shifts: &[f64]) -> Vec<OutputRow> {
+fn compute_series(polygon: &Polygon<f64>, shifts: &[f64], angles: &[f64]) -> Vec<Series> {
+    let mut series: Vec<Series> = Vec::with_capacity(angles.len());
+    for angle in angles {
+        let mut data: Vec<Datum> = Vec::with_capacity(shifts.len());
+
+        for shift in shifts {
+            data.push(compute_datum(polygon, *angle, *shift));
+        }
+        series.push(Series {
+            angle: *angle,
+            data,
+        })
+    }
+    series
+}
+
+fn compute_datum(polygon: &Polygon<f64>, angle: f64, shift: f64) -> Datum {
     let original_area = polygon.signed_area();
-    let mut output: Vec<OutputRow> = Vec::with_capacity(shifts.len());
-    for shift in shifts {
-        let shifted_polygon = polygon.map_coords(|&(x, y)| (x + shift, y + shift));
-        let shifted_area = shifted_polygon.signed_area();
-        let naive_area = get_linestring_area(shifted_polygon.exterior());
-        output.push(OutputRow{
-            shift: *shift,
-            original_area: original_area,
-            shifted_area: shifted_polygon.signed_area(),
-            shift_relative_error: (original_area - shifted_area).abs()/original_area,
-            naive_area_computation_error: (shifted_area - naive_area).abs()/shifted_area
-        });
+
+    let xshift = shift * angle.cos();
+    let yshift = shift * angle.sin();
+    let shifted_polygon = polygon.map_coords(|&(x, y)| (x + xshift, y + yshift));
+    let shifted_area = shifted_polygon.signed_area();
+    let naive_area = get_linestring_area(shifted_polygon.exterior());
+    Datum {
+        shift,
+        original_area,
+        shifted_area: shifted_polygon.signed_area(),
+        shift_relative_error: (original_area - shifted_area).abs() / original_area,
+        naive_area_computation_error: (shifted_area - naive_area).abs() / shifted_area,
     }
-    output
 }
 
-fn print_output(output: &[OutputRow]) {
-    for row in output {
-        println!(
-            "{:.4e}, {:.4e}, {:.4e}, {:.4e}, {:.4e}",
-            row.shift,
-            row.original_area,
-            row.shifted_area,
-           row.shift_relative_error,
-           row.naive_area_computation_error
-        );
-    }
-}
 
 fn get_linestring_area<T>(linestring: &LineString<T>) -> T
 where
