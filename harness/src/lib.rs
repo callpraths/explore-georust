0533b0963641ac2b41b491fd85815f760a86a766
Prathmesh Prabhu <callpraths@gmail.com>
2021-12-09 00:56:34 +0000

    Group runs in an iteration

--- harness/src/lib.rs
+++ harness/src/lib.rs
@@ -17,45 +17,56 @@ pub mod notsofine {
     }
 
     #[derive(Clone, Debug, Serialize)]
-    pub struct Iteration {
-        pub program: String,
-        pub started_at: SystemTime,
-        pub duration: Duration,
+    pub struct BenchmarkResult {
+        pub iterations: Vec<Iteration>,
     }
 
     #[derive(Clone, Debug, Serialize)]
-    pub struct BenchmarkResult {
-        pub runs: Vec<Iteration>,
+    pub struct Iteration {
+        pub i: usize,
+        pub started_at: SystemTime,
+        pub runs: Vec<Run>,
+    }
+
+    #[derive(Clone, Debug, Serialize)]
+    pub struct Run {
+        pub program: String,
+        pub duration: Duration,
     }
 
     pub fn benchmark_run(args: Args) -> BenchmarkResult {
         let mut result: BenchmarkResult = BenchmarkResult {
-            runs: Vec::with_capacity(args.iterations * args.programs.len()),
+            iterations: Vec::with_capacity(args.iterations),
         };
 
         let radix = args.programs.len();
         let mut head: usize = 0;
         for i in 0..args.iterations {
+            let mut iteration = Iteration {
+                i,
+                started_at: SystemTime::now(),
+                runs: Vec::with_capacity(args.programs.len()),
+            };
             println!("Iteration {} of {}", i, args.iterations);
-            for i in 0..radix {
-                result
+            for p in 0..radix {
+                iteration
                     .runs
-                    .push(run_once(&args.programs[(head + i) % radix]));
+                    .push(run_once(&args.programs[(head + p) % radix]));
             }
             head = (head + 1) % radix;
+            result.iterations.push(iteration);
         }
         result
     }
 
-    fn run_once(program: &Box<dyn Program>) -> Iteration {
+    fn run_once(program: &Box<dyn Program>) -> Run {
         let prepared = program.prepare();
-        let wall_clock_start = SystemTime::now();
+
         let start = Instant::now();
         prepared.benchmark_this();
         let end = Instant::now();
-        Iteration {
+        Run {
             program: program.name(),
-            started_at: wall_clock_start,
             duration: end - start,
         }
     }
