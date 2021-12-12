116aaeb84b4642f99e8968be615048a9d0a738b8
Prathmesh Prabhu <callpraths@gmail.com>
2021-12-12 23:17:13 +0000

    Promote area computation analysis

--- harness/src/bin/benchmark_area.rs
+++ harness/src/bin/benchmark_area.rs
@@ -1,30 +1,34 @@
 use clap::Parser;
 use criterion;
-use geo::{algorithm::area::Area, Coordinate, Polygon};
-use geos::{Geom, Geometry};
-use harness::notsofine::*;
-use std::{f64::consts::PI, fs::File, io::Write};
+use geo::{algorithm::area::Area, Geometry};
+use geos::{self, Geom};
+use harness::{
+    data::{self, MultiPolygonPack},
+    notsofine::*,
+};
+
+use std::{fs::File, io::Write};
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct CLIArgs {
-    /// Name of the person to greet
+    #[clap(short, long)]
+    geojson_file: String,
     #[clap(short, long)]
     out_file: String,
-
     #[clap(short, long)]
     iterations: usize,
 }
 
 const NUM_COMPUTATIONS: usize = 100_000;
 
-fn geo_area(mut polygon: Polygon<f64>) {
+fn geo_area(mut polygon: Geometry<f64>) {
     for _ in 0..NUM_COMPUTATIONS {
         criterion::black_box(criterion::black_box(&mut polygon).signed_area());
     }
 }
 
-fn geos_area(mut g: Geometry) {
+fn geos_area(mut g: geos::Geometry) {
     for _ in 0..NUM_COMPUTATIONS {
         criterion::black_box(criterion::black_box(&mut g).area().unwrap());
     }
@@ -32,37 +36,21 @@ fn geos_area(mut g: Geometry) {
 
 fn main() {
     let args = CLIArgs::parse();
-
-    let polygon = {
-        const NUM_VERTICES: usize = 10;
-        const ANGLE_INC: f64 = 2. * PI / NUM_VERTICES as f64;
-        const RADIUS: f64 = 10.;
-
-        Polygon::new(
-            (0..NUM_VERTICES)
-                .map(|i| {
-                    let angle = i as f64 * ANGLE_INC;
-                    Coordinate {
-                        x: RADIUS * angle.cos(),
-                        y: RADIUS * angle.sin(),
-                    }
-                })
-                .collect::<Vec<_>>()
-                .into(),
-            vec![],
-        )
-    };
+    let MultiPolygonPack {
+        geo: geo_mp,
+        geos: geos_mp,
+    } = data::load_multipolygon_pack(&args.geojson_file);
 
     let result = benchmark_run(Args {
         programs: vec![
-            simple::program_for_fn_with_arg("geo", geo_area, polygon.clone()),
-            simple::program_for_fn_with_arg("geos", geos_area, polygon.try_into().unwrap()),
+            simple::program_for_fn_with_arg("geo", geo_area, geo_mp),
+            simple::program_for_fn_with_arg("geos", geos_area, geos_mp),
         ],
         iterations: args.iterations,
     });
 
-    let mut ofile = File::create(args.out_file).unwrap();
-    ofile
+    File::create(args.out_file)
+        .unwrap()
         .write_all(serde_json::to_string_pretty(&result).unwrap().as_bytes())
         .unwrap();
 }
