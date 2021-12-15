55236673a0065d1151b37c9b886a9ceae9a98d14
Prathmesh Prabhu <callpraths@gmail.com>
2021-12-15 00:05:48 +0000

    Add centroid benchmark

---
+++ harness/src/bin/benchmark_centroid.rs
@@ -0,0 +1,62 @@
+use clap::Parser;
+use criterion;
+use geo::{algorithm::centroid::Centroid, Geometry};
+use geos::{self, Geom};
+use harness::{
+    data::{self, MultiPolygonPack},
+    notsofine::*,
+};
+
+use std::{fs::File, io::Write, time::Duration};
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct CLIArgs {
+    #[clap(short, long)]
+    geojson_file: String,
+    #[clap(short, long)]
+    out_file: String,
+}
+
+const NUM_COMPUTATIONS: usize = 100_000;
+
+fn geo_centroid(mut polygon: Geometry<f64>) {
+    for _ in 0..NUM_COMPUTATIONS {
+        criterion::black_box(criterion::black_box(&mut polygon).centroid());
+    }
+}
+
+fn geos_centroid(mut g: geos::Geometry) {
+    for _ in 0..NUM_COMPUTATIONS {
+        criterion::black_box(criterion::black_box(&mut g).get_centroid().unwrap());
+    }
+}
+
+fn main() {
+    let args = CLIArgs::parse();
+    let MultiPolygonPack {
+        geo: geo_mp,
+        geos: geos_mp,
+    } = data::load_multipolygon_pack(&args.geojson_file);
+
+    println!("geo centroid: {:#?}", geo_mp.centroid());
+    println!(
+        "geos centroid: {:#?}",
+        geos_mp.get_centroid().unwrap().to_wkt().unwrap()
+    );
+
+    let result = benchmark_run(Args {
+        programs: vec![
+            simple::program_for_fn_with_arg("geo", geo_centroid, geo_mp),
+            simple::program_for_fn_with_arg("geos", geos_centroid, geos_mp),
+        ],
+        iterations: 210,
+        discard_leading: Some(10),
+        pause: Some(Duration::new(3, 0)),
+    });
+
+    File::create(args.out_file)
+        .unwrap()
+        .write_all(serde_json::to_string_pretty(&result).unwrap().as_bytes())
+        .unwrap();
+}
